5d42257d76fc0871d0a556a147d82544f4197735
Jonni Silva <j0nn1@protonmail.com>
2023-10-21 19:19:11 -0700

    ok

---
+++ Jogo_da_forca.py
@@ -0,0 +1,26 @@
+from palavraforca import palavra
+
+letras_usuario = []
+chances = 12
+ganhou = False
+
+while True:
+    # criar a nossa lógica do jogo
+    for letra in palavra:
+        if letra.lower() not in letras_usuario:
+            print("_", end=" ")
+        else:
+            print(letra, end=" ")
+    print(f"Você tem {chances} chances!")
+    tentativa = input("Escolha uma letra: ")
+    letras_usuario.append(tentativa.lower())
+    if tentativa.lower() not in palavra.lower():
+        chances -= 1
+
+    ganhou = True
+    for letra in palavra:
+        if letra.lower() not in letras_usuario:
+            ganhou = False
+
+    if chances == 0 or ganhou:
+        break
