4af73206777f2894614d8165d5749f8bafab94a3
Jonni Silva <j0nn1@protonmail.com>
2023-10-21 18:04:31 -0700

    ok

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ print('M4r71n5')
 print('51lv4') #TODO: Aprendendo coisas bacanas
 print('51lv4') #TODO: Aprendendo coisas bacanas
 # Aprendendo esta linda linguagem de Programa;'ao chamada PYTHON!!!
+# Acho que aprendi
